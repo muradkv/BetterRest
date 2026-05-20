a8b971d422c402b03b18bafd4410b508adea40a2
murad <muradkv@gmail.com>
2026-05-20 18:33:16 +0300

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by murad on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
